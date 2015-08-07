b6f8ff23c260a02a8837d477c79cda8ae7eea1bb
David Toomey <d.toomey@temple.edu>
2015-08-07 15:24:02 -0400

    final changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,16 @@ def show
   if @user.current_caloric_intake > @user.basal_metabolic_rate 
       @word = "more"
       @word2 = "gain"
-      @word3 = "Gain"
+      @word3 = "Consume"
     else
       @word = "fewer"
-      @word2 = "burn"
+      @word2 = "lose"
       @word3 = "Burn"
     end
   if @user.rest_loss_gain == 1
     @day = "day"
   else 
     @day = "days"
-  else
-    @day = 'days'
   end
 end
 
