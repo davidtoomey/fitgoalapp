69b32f15e0f5654715e936af707bdf81ca1a23da
Patrick Keenan <pgk4526@gmail.com>
2015-08-07 15:07:22 -0400

    merge

--- app/assets/javascripts/show.js
+++
@@ -1,1 +0,0 @@
-$('form').trigger('submit.rails');
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,11 @@ def show
   if @user.current_caloric_intake > @user.basal_metabolic_rate 
       @word = "more"
       @word2 = "gain"
+      @word3 = "Gain"
     else
       @word = "fewer"
       @word2 = "burn"
+      @word3 = "Burn"
     end
   if @user.rest_loss_gain == 1
     @day = "day"
